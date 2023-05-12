57ea5a56a276d9b60cb5f672304175f8dd4eff68
baiqi <443543832@qq.com>
2023-05-12 14:01:25 +0800

    feat: 项目初始化

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  htmlWhitespaceSensitivity: 'strict',
+  vueIndentScriptAndStyle: true,
+  useTabs: false,
+  trailingComma: 'es5',
+  printWidth: 120,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+};
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Arco-Design-Pro-TMP
-Arco Design Pro Template
+
+> Arco Design Pro Template，使用 [arco-cli](https://github.com/arco-design/arco-cli) 脚手架初始化的 Arco Design 最佳实践项目模板，结合自己的需求做了调整
